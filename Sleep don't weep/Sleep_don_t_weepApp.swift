55ca685300dbc5298a8a5c9a52283ca35157cf1c
Filip <filip@kopiujwklej.net>
2024-02-25 18:39:31 +0100

    Initial Commit

---
+++ Sleep don't weep/Sleep_don_t_weepApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sleep_don_t_weepApp.swift
+//  Sleep don't weep
+//
+//  Created by Filip Pokłosiewicz on 25/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Sleep_don_t_weepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
